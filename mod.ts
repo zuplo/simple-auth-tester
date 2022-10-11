3246225ed6aba061fdb5b0bdb5aa80976b3045b9
Nathan Totten <nathan@ntotten.com>
2022-10-11 14:32:20 -0400

    route changes

--- mod.ts
+++ mod.ts
@@ -1,96 +1,114 @@
 // Start listening on port 8080 of localhost.
 import { serve } from "https://deno.land/std@0.158.0/http/server.ts";
 import { config } from "https://deno.land/std@0.158.0/dotenv/mod.ts";
+import {
+  RouteHandler,
+  Router,
+} from "https://deno.land/x/tinyrouter@1.1.0/mod.ts";
 
-const env = await config();
+interface Env {
+  AUTH0_DOMAIN: string;
+  AUTH0_CLIENT_ID: string;
+  AUTH0_CLIENT_SECRET: string;
+  AUTH0_SCOPE: string;
+  AUDIENCE: string;
+  BASE_URL: string;
+}
+
+const env = (await config()) as unknown as Env;
 
 const port = 8080;
 
-serve(handler, { port });
+const authorize: RouteHandler = (request) => {
+  const authUrl = new URL(`https://${env.AUTH0_DOMAIN}/authorize`);
+  authUrl.searchParams.set("response_type", "code");
+  authUrl.searchParams.set("redirect_uri", `${env.BASE_URL}/callback`);
+  authUrl.searchParams.set("scope", env.AUTH0_SCOPE);
+  authUrl.searchParams.set("client_id", env.AUTH0_CLIENT_ID);
+  if (env.AUDIENCE) {
+    authUrl.searchParams.set("audience", env.AUDIENCE);
+  }
 
-async function handler(request: Request) {
+  return Response.redirect(authUrl);
+};
+
+const logout: RouteHandler = (request) => {
+  return Response.redirect(
+    `https://${env.AUTH0_DOMAIN}/v2/logout?federated&returnTo=${encodeURI(
+      env.BASE_URL
+    )}`
+  );
+};
+
+const callback: RouteHandler = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/") {
-    const authUrl = new URL(`https://${env.AUTH0_DOMAIN}/authorize`);
-    authUrl.searchParams.set("response_type", "code");
-    authUrl.searchParams.set(
-      "redirect_uri",
-      `http://localhost:${port}/callback`,
-    );
-    authUrl.searchParams.set("scope", env.AUTH0_SCOPE);
-    authUrl.searchParams.set("client_id", env.AUTH0_CLIENT_ID);
-    if (env.AUDIENCE) {
-      authUrl.searchParams.set("audience", env.AUDIENCE);
-    }
+  const code = url.searchParams.get("code");
+  if (!code) {
+    return new Response("Code not set", { status: 500 });
+  }
 
-    return Response.redirect(authUrl);
-  } else if (url.pathname === "/logout") {
-    return Response.redirect(`https://${env.AUTH0_DOMAIN}/v2/logout?federated`);
-  } else if (url.pathname === "/callback") {
-    const code = url.searchParams.get("code");
-    if (!code) {
-      return new Response("Code not set", { status: 500 });
-    }
+  const body = new URLSearchParams();
+  body.set("grant_type", "authorization_code");
+  body.set("client_id", env.AUTH0_CLIENT_ID);
+  body.set("client_secret", env.AUTH0_CLIENT_SECRET);
+  body.set("code", code);
+  body.set("redirect_uri", `${env.BASE_URL}/callback`);
 
-    const body = new URLSearchParams();
-    body.set("grant_type", "authorization_code");
-    body.set("client_id", env.AUTH0_CLIENT_ID);
-    body.set("client_secret", env.AUTH0_CLIENT_SECRET);
-    body.set("code", code);
-    body.set("redirect_uri", `http://localhost:${port}/callback`);
+  const response = await fetch(`https://${env.AUTH0_DOMAIN}/oauth/token`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    body,
+  });
 
-    const response = await fetch(`https://${env.AUTH0_DOMAIN}/oauth/token`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-      },
-      body,
-    });
-
-    const data = await response.json();
-    if (!response.ok) {
-      const html = `
+  const data = await response.json();
+  if (!response.ok) {
+    const html = `
       <h1>Error Response</h1>
+      <a href="${env.BASE_URL}/logout">Logout</a>
       <pre>${JSON.stringify(data, null, 2)}</pre>
-      <a href="http://localhost:${port}/">Refresh</a>
   `;
-      return sendResponse(html, 200);
-    }
+    return sendResponse(html, 200);
+  }
 
-    console.log(data);
+  const idToken: string | undefined = data.id_token;
+  const accessToken: string | undefined = data.access_token;
 
-    const idToken: string | undefined = data.id_token;
-    const accessToken: string | undefined = data.access_token;
-
-    let html = `
+  let html = `
     <h1>Authentication Result</h1>
+    <a href="${env.BASE_URL}/logout">Logout</a>
     <h2>Token Response</h2>
     <pre>${JSON.stringify(data, null, 2)}</pre>`;
 
-    // Get Profile
-    if (accessToken) {
-      const profile = await getProfile(accessToken);
-      html += `
+  // Get Profile
+  if (accessToken) {
+    const profile = await getProfile(accessToken);
+    html += `
       <hr/>
       <h2>Profile</h2>
       <pre>${JSON.stringify(profile, null, 2)}</pre>
       `;
-    }
-
-    if (idToken) {
-      html += getTokenHtml("ID Token", idToken);
-    }
-
-    if (accessToken && !accessToken.includes("..")) {
-      html += getTokenHtml("Access Token", accessToken);
-    }
-
-    return sendResponse(html, 200);
   }
-  return new Response("Not Found", {
-    status: 404,
-  });
-}
+
+  if (idToken) {
+    html += getTokenHtml("ID Token", idToken);
+  }
+
+  if (accessToken && !accessToken.includes("..")) {
+    html += getTokenHtml("Access Token", accessToken);
+  }
+
+  return sendResponse(html, 200);
+};
+
+const router = new Router();
+router.get("/", authorize);
+router.get("/logout", logout);
+router.get("/callback", callback);
+router.all("*", () => new Response("Not found", { status: 404 }));
+
+serve((request) => router.handler(request), { port });
 
 function getTokenHtml(tokenName: string, token: string) {
   const header = JSON.parse(decodeJwt(token.split(".")[0]));
@@ -110,6 +128,9 @@ function getTokenHtml(tokenName: string, token: string) {
 }
 
 async function getProfile(accessToken: string) {
+  if (!env.AUTH0_SCOPE?.includes("oidc")) {
+    return {};
+  }
   const response = await fetch(`https://${env.AUTH0_DOMAIN}/userinfo`, {
     headers: {
       authorization: `Bearer ${accessToken}`,
@@ -130,7 +151,7 @@ function b64DecodeUnicode(str: string) {
         code = "0" + code;
       }
       return "%" + code;
-    }),
+    })
   );
 }
 
